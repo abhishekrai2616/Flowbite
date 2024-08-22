d6eb83420849efd690095b23c2bfc0a7eab874b0
AbhiBrimo <abhishekrai2616@gmail.com>
2024-08-22 10:10:50 +0530

    first

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  ssr: false,
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="relative">
+            <Navbar />
+        </div>
+        <div class="bg-gray-400 relative">
+            <Dropdown />
+        </div>
+        <div class="relative">
+            <Sidebar />
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { initFlowbite } from 'flowbite'
+    onMounted(() => {
+        initFlowbite();
+    })
+</script>
+
+<style scoped>
+
+</style>
